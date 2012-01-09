1aa177e949ba58ecc2f8dc4b660755421b13a16f
Dmitry Rozhkov <dmitry.rojkov@gmail.com>
2012-01-09 15:38:35 +0000

    fully cover VmGuest.__init__() with unit tests

--- tests/test_vmsetup.py
+++ tests/test_vmsetup.py
@@ -1,6 +1,6 @@
 import unittest
 
-from ConfigParser import ConfigParser
+from ConfigParser import ConfigParser, NoOptionError
 
 from virtlib.vmsetup import VMGuest
 
@@ -11,13 +11,22 @@ class TestVMGuest(unittest.TestCase):
         config = ConfigParser()
         config.add_section("vmguest_test")
         config.set("vmguest_test", "tpl_type", "testhost")
+        config.set("vmguest_test", "tpl_dir", "/tmp")
         config.add_section("tpl_testhost")
         config.set("tpl_testhost", "vm_dir", "/tmp")
-        config.set("tpl_testhost", "tarball", "/tmp/1.tgz")
+        config.set("tpl_testhost", "tarball", "tests/test_vmsetup.py")
         self.config = config
 
     def test_init(self):
-        vmguest = VMGuest("test", self.config)
+        # no templates defined
+        VMGuest("test", self.config)
+        VMGuest("test", self.config, {})
+        # some templates defined
+        self.config.set("tpl_testhost", "tpls", "tpl1,tpl2")
+        self.config.set("tpl_testhost", "tpl1", "fake_path1")
+        self.assertRaises(NoOptionError, VMGuest, "test", self.config)
+        self.config.set("tpl_testhost", "tpl2", "fake_path2")
+        VMGuest("test", self.config)
 
 if __name__ == "__main__":
     unittest.main()
