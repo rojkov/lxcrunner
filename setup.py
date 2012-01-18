c66116b25a296413811cf05311f85c1d940cd3db
Mikhail Sobolev <mss@mawhrin.net>
2012-01-18 14:42:39 +0200

    change console script name

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     entry_points={
         'console_scripts':
             [
-                'runtests = virtlib.runtests:main',
+                'virtlib-cli = virtlib.runtests:main',
             ],
     },
     test_suite = "tests"
